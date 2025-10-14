149edc4a1b64fa2d79490ba277e163d6be4693d3
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-14 13:33:04 -0600

    added API folder and books.js file to fetch book list from API

--- src/Auth/AuthContext.jsx
+++ src/Auth/AuthContext.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
 const API = import.meta.env.VITE_API;
 const AuthContext = createContext();
@@ -7,6 +6,40 @@ const AuthContext = createContext();
 function AuthProvider({ children }) {
   const [token, setToken] = useState();
 
+  const register = async (credentials) => {
+    const res = await fetch(API + "/users/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const result = await res.json();
+    if (!res.ok) {
+      throw Erorr(result.message);
+    }
+    setToken(result.token);
+  };
+
+  const login = async (credentials) => {
+    const res = await fetch(API + "/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const result = await res.json();
+    if (!res.ok) {
+      throw Erorr(result.message);
+    }
+    setToken(result.token);
+  };
+
+  const logout = () => setToken(null);
+
+  const value = {
+    token,
+    login,
+    register,
+    logout,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 export default AuthProvider;
