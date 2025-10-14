e8cb190f73f7ceaf9a4fd97619e4a10d4389ad31
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-14 13:09:12 -0600

    added AuthProvider function and useAuth function to create Authcontext for login and register functions.

--- src/Auth/AuthContext.jsx
+++ src/Auth/AuthContext.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-function AuthContext() {
-  return <div>AuthContext</div>;
+const API = import.meta.env.VITE_API;
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [token, setToken] = useState();
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
+export default AuthProvider;
 
-export default AuthContext;
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw Error("useAuth must be used within AuthProvider");
+  return context;
+}
