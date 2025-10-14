b723a0d82fb3fb3e5b4ce0fc70112490a7c92b77
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-14 15:42:22 -0600

    Added Books folder, with BookPage, BookDetails and Booklist components. Created BookPage logic and UI. Starting to add APP routes and imports to setup page routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { Route, Routes } from "react-router";
+import BookPage from "./Books/BookPage";
+import Layout from "./Layout/layout";
+import Register from "./Auth/register";
+import Login from "./Auth/Login";
+
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route>
+        <Route></Route>
+      </Route>
+    </Routes>
+  );
 }
---
+++ src/Books/BookPage.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { getBooks } from "../API/Books";
+import BookList from "./BookList";
+
+export default function BookPage() {
+  const [books, setBooks] = useState();
+
+  const syncBooks = async () => {
+    const data = await getBooks();
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    syncBooks();
+  }, []);
+
+  return (
+    <>
+      <h1>Catalog</h1>
+      <label>
+        <input type="text" name="searchbar" id="" />, <button>Search</button>
+      </label>
+      <BookList books={books} syncBooks={syncBooks} />
+    </>
+  );
+}
