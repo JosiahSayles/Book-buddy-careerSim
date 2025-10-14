c6580f16ee74ec4f06accfd29e2d0ea0a969feb1
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-14 13:44:52 -0600

    added getBooks function to books.js to request all books list.

--- src/API/books.js
+++ src/API/books.js
@@ -1,1 +1,12 @@
 const API = import.meta.env.VITE_API;
+
+export async function getBooks() {
+  try {
+    const res = await fetch(API + "./books");
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
