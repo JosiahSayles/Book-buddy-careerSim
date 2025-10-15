ad0e9ce2403916a9864a7aad3c7d7566e9a9798b
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-15 14:06:56 -0600

    added Reservations component and reserveABook function to that component. to pull all reservations from API and to send post request to reserve books

--- src/Books/BookPage.jsx
+++ src/Books/BookPage.jsx
@@ -18,7 +18,7 @@ export default function BookPage() {
     <>
       <h1>Catalog</h1>
       <label>
-        <input type="text" name="searchbar" id="" text="search for a book" />
+        <input type="text" name="searchbar" id="" />
         <button>Search</button>
       </label>
       <BookList books={books} syncBooks={syncBooks} />
